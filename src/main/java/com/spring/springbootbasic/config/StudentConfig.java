3a992b0c7cc003a095a34c8f2e185ebd5814357f
nagarjuna_amboji <contactnagarjuna247@gmail.com>
2022-03-21 22:43:58 +0530

    Committed the Repository files

---
+++ src/main/java/com/spring/springbootbasic/config/StudentConfig.java
@@ -0,0 +1,39 @@
+package com.spring.springbootbasic.config;
+
+import com.spring.springbootbasic.bean.Student;
+import com.spring.springbootbasic.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+
+            Student arjun = new Student(
+                            "Arjuna",
+                            LocalDate.of(1990, Month.JULY, 17),
+                            31,
+                            "nagarjuna.a@gmail.com"
+                    );
+            Student vani = new Student(
+                    "Vani",
+                    LocalDate.of(1992, Month.APRIL, 22),
+                    28,
+                    "amboji.vani@gmail.com"
+            );
+
+            studentRepository.saveAll(
+                    List.of(arjun, vani)
+            );
+
+        };
+    }
+}
--- src/main/java/com/spring/springbootbasic/service/StudentService.java
+++ src/main/java/com/spring/springbootbasic/service/StudentService.java
@@ -1,6 +1,8 @@
 package com.spring.springbootbasic.service;
 
 import com.spring.springbootbasic.bean.Student;
+import com.spring.springbootbasic.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,15 +11,14 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Arjuna",
-                        LocalDate.of(1990, 07, 17),
-                        21,
-                        "nagarjuna.a@gmail.com"
-                )
-        );
+        return studentRepository.findAll();
     }
 }
